9959b6b0aa208637b225774a32e09df1f3c10216
luizarsantana <luiza.santana@sou.inteli.edu.br>
2023-10-17 11:54:50 -0300

    código_morse

---
+++ SEMANA_01/01_CODIGOS/atividade2_sala/atividade2_sala.ino
@@ -0,0 +1,42 @@
+void setup() {
+  pinMode(15, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(15, HIGH);
+  delay(400); // uma unidade
+  digitalWrite(15, LOW);
+  delay(400);
+  digitalWrite(15, HIGH);
+  delay(400);
+  digitalWrite(15, LOW);
+  delay(400);
+  digitalWrite(15, HIGH);
+  delay(400);
+  digitalWrite(15, LOW);
+  delay(1200); // três unidades
+  digitalWrite(15, HIGH);
+  delay(1200);
+  digitalWrite(15, LOW);
+  delay(400);
+  digitalWrite(15, HIGH);
+  delay(1200);
+  digitalWrite(15, LOW);
+  delay(400);
+  digitalWrite(15, HIGH);
+  delay(1200);
+  digitalWrite(15, LOW);
+  delay(1200);
+  digitalWrite(15, HIGH);
+  delay(400);
+  digitalWrite(15, LOW);
+  delay(400);
+  digitalWrite(15, HIGH);
+  delay(400);
+  digitalWrite(15, LOW);
+  delay(400);
+  digitalWrite(15, HIGH);
+  delay(400);
+  digitalWrite(15, LOW);
+  delay(2800); // sete unidades
+}
